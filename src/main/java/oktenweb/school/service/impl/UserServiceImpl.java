8a46d0f40bb2ac130d47c5e24b67d65cc4670416
YuraVolokhin <yura.volohin@gmail.com>
2018-11-23 15:32:22 +0200

    UserServiceImpl

--- src/main/java/oktenweb/school/service/impl/UserServiceImpl.java
+++ src/main/java/oktenweb/school/service/impl/UserServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 //import java.lang.String;
 
-
+//change
 
 //change
 
