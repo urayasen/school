aa10659e0372512a1f471e8759c2351962f09ef5
YuraVolokhin <yura.volohin@gmail.com>
2018-11-23 15:32:22 +0200

    UserServiceImpl

--- src/main/java/oktenweb/school/service/impl/UserServiceImpl.java
+++ src/main/java/oktenweb/school/service/impl/UserServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-
+//change
 
 @Service
 public class UserServiceImpl implements UserService {
