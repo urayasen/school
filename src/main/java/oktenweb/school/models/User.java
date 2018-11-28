c79b458c9c7e779cf3b989b1e4356be287d10c73
okten119 <root>
2018-11-28 15:55:58 +0200

    UserService

--- src/main/java/oktenweb/school/models/User.java
+++ src/main/java/oktenweb/school/models/User.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @Entity
 public class User implements UserDetails {
-
+//xgxgxghgh
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private int id;
--- src/main/java/oktenweb/school/models/custom/Students.java
+++ src/main/java/oktenweb/school/models/custom/Students.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 
 @Entity
 public class Students {
-
+//ghxghxfghgh
 
     /*--------------------------------------------------------------------------------------------------*/
     @Id
