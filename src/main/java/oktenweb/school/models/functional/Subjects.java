10bf43ee81b2777ad4ca1b8ebc4d6094d616d970
YuraVolokhin <yura.volohin@gmail.com>
2018-12-04 14:13:57 +0200

    add functional Classes and relationships1

--- src/main/java/oktenweb/school/models/functional/Subjects.java
+++ src/main/java/oktenweb/school/models/functional/Subjects.java
@@ -48,7 +48,7 @@ public class Subjects {
             cascade = CascadeType.ALL,
             mappedBy = "subjects"
     )
-    private ClassJournal classJournal;
+    private List<ClassJournal> classJournal = new ArrayList<>();
 
 
 
@@ -70,11 +70,11 @@ public class Subjects {
         this.classteachers = classteachers;
     }
 
-    public ClassJournal getClassJournal() {
+    public List<ClassJournal> getClassJournal() {
         return classJournal;
     }
 
-    public void setClassJournal(ClassJournal classJournal) {
+    public void setClassJournal(List<ClassJournal> classJournal) {
         this.classJournal = classJournal;
     }
 
