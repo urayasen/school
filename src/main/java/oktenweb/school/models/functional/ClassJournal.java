1750c4f8ff7d53f9f8da126c8dd23b919a7ad409
urayasen <urayasen@gmail.com>
2018-12-07 13:33:28 +0200

    change before role work

---
+++ src/main/java/oktenweb/school/models/functional/ClassJournal.java
@@ -0,0 +1,142 @@
+package oktenweb.school.models.functional;
+
+import oktenweb.school.models.custom.Classteachers;
+import oktenweb.school.models.custom.Deputy;
+import oktenweb.school.models.custom.Students;
+import oktenweb.school.models.custom.Teachers;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ClassJournal {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String dateMark;
+    private String marks;
+
+
+
+    @Autowired
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private Subjects subjects;
+
+
+    @Autowired
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private Students students;
+
+
+    @Autowired
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private Deputy deputy;
+
+
+    @Autowired
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private Classteachers classteachers;
+
+    @Autowired
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private Teachers teachers;
+
+
+    public Deputy getDeputy() {
+        return deputy;
+    }
+
+    public void setDeputy(Deputy deputy) {
+        this.deputy = deputy;
+    }
+
+    public Classteachers getClassteachers() {
+        return classteachers;
+    }
+
+    public void setClassteachers(Classteachers classteachers) {
+        this.classteachers = classteachers;
+    }
+
+    public Students getStudents() {
+        return students;
+    }
+
+    public void setStudents(Students students) {
+        this.students = students;
+    }
+
+
+    public Subjects getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(Subjects subjects) {
+        this.subjects = subjects;
+    }
+
+    public Teachers getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(Teachers teachers) {
+        this.teachers = teachers;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDateMark() {
+        return dateMark;
+    }
+
+    public void setDateMark(String dateMark) {
+        this.dateMark = dateMark;
+    }
+
+    public String getMarks() {
+        return marks;
+    }
+
+    public void setMarks(String marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassJournal{" +
+                "id=" + id +
+                ", dateMark='" + dateMark + '\'' +
+                ", marks='" + marks + '\'' +
+                ", subjects=" + subjects +
+                ", students=" + students +
+                ", deputy=" + deputy +
+                ", classteachers=" + classteachers +
+                ", teachers=" + teachers +
+                '}';
+    }
+}
---
+++ src/main/java/oktenweb/school/models/functional/Subjects.java
@@ -0,0 +1,135 @@
+package oktenweb.school.models.functional;
+
+import oktenweb.school.models.custom.Classteachers;
+import oktenweb.school.models.custom.Deputy;
+import oktenweb.school.models.custom.Teachers;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+public class Subjects {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @Autowired
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private List<Classes> classes = new ArrayList<>();
+
+
+    @Autowired
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private Teachers teachers;
+
+    @Autowired
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private Deputy deputy;
+
+
+    @Autowired
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL,
+            mappedBy = "subjects"
+    )
+    private List<ClassJournal> classJournal = new ArrayList<>();
+
+
+
+    @Autowired
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private Classteachers classteachers;
+
+
+
+
+    public Classteachers getClassteachers() {
+        return classteachers;
+    }
+
+    public void setClassteachers(Classteachers classteachers) {
+        this.classteachers = classteachers;
+    }
+
+    public List<ClassJournal> getClassJournal() {
+        return classJournal;
+    }
+
+    public void setClassJournal(List<ClassJournal> classJournal) {
+        this.classJournal = classJournal;
+    }
+
+    public List<Classes> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<Classes> classes) {
+        this.classes = classes;
+    }
+
+    public Teachers getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(Teachers teachers) {
+        this.teachers = teachers;
+    }
+
+    public Deputy getDeputy() {
+        return deputy;
+    }
+
+    public void setDeputy(Deputy deputy) {
+        this.deputy = deputy;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Subjects{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", classes=" + classes +
+                ", teachers=" + teachers +
+                ", deputy=" + deputy +
+                ", classJournal=" + classJournal +
+                ", classteachers=" + classteachers +
+                '}';
+    }
+}
+
+
