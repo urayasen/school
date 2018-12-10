50564421335bce284129203b669563af7b4a0fbf
urayasen <urayasen@gmail.com>
2018-12-10 15:45:48 +0200

    change before role work111

---
+++ src/main/java/oktenweb/school/dao/functionalDAO/SubjectsDAO.java
@@ -0,0 +1,8 @@
+package oktenweb.school.dao.functionalDAO;
+
+import oktenweb.school.models.functional.Subjects;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectsDAO extends JpaRepository<Subjects, Integer> {
+    //fgfgdfg
+}
